fa25d90439bd3dfff6d79cc6ee469ce07524ebb0
Magnus Österberg <magnus_osterberg@hotmail.com>
2017-11-24 15:26:43 +0100

    c#

---
+++ C#/misc/delegates/NumberChanger/NumberChanger/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+delegate int NumberChanger(int n);
+
+namespace DelegateAppl
+{
+
+	class TestDelegate
+	{
+		static int num = 10;
+
+		public static int AddNum(int p)
+		{
+			num += p;
+			return num;
+		}
+
+		public static int MultNum(int q)
+		{
+			num *= q;
+			return num;
+		}
+
+		public static int getNum()
+		{
+			return num;
+		}
+
+		static void Main(string[] args)
+		{
+			//create delegate instances
+			NumberChanger nc1 = new NumberChanger(AddNum);
+			NumberChanger nc2 = new NumberChanger(MultNum);
+			NumberChanger nc3 = new NumberChanger(AddNum);
+
+			//calling the methods using the delegate objects
+			nc1(25);
+			Console.WriteLine("Value of Num: {0}", getNum());
+
+			nc2(5);
+			Console.WriteLine("Value of Num: {0}", getNum());
+
+			nc3(1);
+			Console.WriteLine("Value of Num: {0}", getNum());
+
+			Console.ReadKey();
+		}
+	}
+}
---
+++ C#/misc/inheritance/InheritanceApplication/InheritanceApplication/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace RectangleApplication
+{
+
+	class Rectangle
+	{
+		//member variables
+		protected double length;
+		protected double width;
+
+		public Rectangle(double l, double w)
+		{
+			length = l;
+			width = w;
+		}
+
+		public double GetArea()
+		{
+			return length * width;
+		}
+
+		public void Display()
+		{
+			Console.WriteLine("Length: {0}", length);
+			Console.WriteLine("Width: {0}", width);
+			Console.WriteLine("Area: {0}", GetArea());
+		}
+	}
+
+
+	class Tabletop : Rectangle
+	{	
+				
+		public Tabletop(double l, double w) : base(l, w) { }
+
+		public double GetCost()
+		{
+			return GetArea() * 70;			
+		}
+
+		public new void Display()
+		{
+			base.Display();
+			Console.WriteLine("Cost: {0}", GetCost());
+		}
+	}
+
+	class ExecuteRectangle
+	{
+
+		static void Main(string[] args)
+		{
+			Tabletop t = new Tabletop(4.5, 7.5);
+			t.Display();
+			Console.ReadLine();
+		}
+	}
+}
